b2b6a544f7a4eed84325ba3213613401076e459d
Philip Akca <philip_akca@hotmail.com>
2022-10-29 15:13:54 +0200

    Ursprunglig

---
+++ src/MainPussel.java
@@ -0,0 +1,8 @@
+public class MainPussel {
+    public static void main(String[] args) {
+        /*
+        Här har vi kanske en kort konstruktor där vi kan ändra meddelanden och kanske antal rutor, men all
+        huvudkod ska vara i andra klasser
+         */
+    }
+}
---
+++ src/Pussel.java
@@ -0,0 +1,8 @@
+public class Pussel {
+    /*
+    Här skapar vi ett objekt av ett pussel. Ska finnas metoder för hur många rutor som skapas,
+    slumpmässiga utlägget, hur man vinner samt action listener
+    för hur allt ska utföras med klickandet.
+    Visar det sig att vi kan koppla loss vissa delar till ett ny klass så kan vi skapa en till
+     */
+}
